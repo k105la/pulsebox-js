a8f2d8e8ee8d1e74dba69c10bffb99c845639efe
Akil M Hylton <hyltonakil@gmail.com>
2020-07-03 02:30:20 -0400

    Update index.js
    Added your firebase auth

--- public/index.js
+++ public/index.js
@@ -1,12 +1,10 @@
 let firebaseConfig = {
-  apiKey: "AIzaSyBoCLNFNU2-_J6NQtbLD7GGy30zRvkzBmk",
-  authDomain: "pulse-box.firebaseapp.com",
-  databaseURL: "https://pulse-box.firebaseio.com",
-  projectId: "pulse-box",
-  storageBucket: "pulse-box.appspot.com",
-  messagingSenderId: "118237267477",
-  appId: "1:118237267477:web:ccee5e3ab7928a8ce4301c",
-  measurementId: "G-0Q1RSLYZVN",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  appId: "",
 };
 
 firebase.initializeApp(firebaseConfig);
